fe7e1cefdaa321c59fe26f36b1b4b4d171ebdc2d
binodgupta <binodgupta010>
2019-04-02 03:17:41 -1200

    validation

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input , Output , EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,26 +12,35 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
  @Input()  valueFromHome: any;
  @Output()  cancelRegister = new EventEmitter();
-
+ registerForm: FormGroup;
  model: any = {};
 
   constructor(private authService: AuthService , private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      userName: new FormControl('' , Validators.required),
+      password: new FormControl('' , [Validators.required , Validators.minLength(4),
+      Validators.maxLength(8)]),
+      confirmPassword: new FormControl('' , Validators.required)
+    }, this.passwordMatchValidator);
   }
 
+  passwordMatchValidator(fg: FormGroup) {
+   return fg.get('password').value === fg.get('confirmPassword').value ? null : { 'mismatch' : true};
+  }
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        this.alertifyService.success('register successfully');
-        console.log('register successfully');
-      }, error => {
-        this.alertifyService.error('register failed');
-        console.log('register failed');
-      }
-    );
-
-     console.log(this.model);
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertifyService.success('register successfully');
+    //     console.log('register successfully');
+    //   }, error => {
+    //     this.alertifyService.error('register failed');
+    //     console.log('register failed');
+    //   }
+    // );
+    //  console.log(this.model);
+    console.log(this.registerForm.value);
   }
 
   cancel() {
