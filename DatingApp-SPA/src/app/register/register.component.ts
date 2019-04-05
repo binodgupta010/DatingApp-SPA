b967dc7181ba83ee1bec6b2e3c8567b314e9e9cb
Binod Kumar Gupta <binod.gupta@tavant.com>
2019-04-05 18:07:41 +0530

    work on register comp

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input , Output , EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 
 @Component({
@@ -15,19 +15,34 @@ export class RegisterComponent implements OnInit {
  registerForm: FormGroup;
  model: any = {};
 
-  constructor(private authService: AuthService , private alertifyService: AlertifyService) { }
+  constructor(private authService: AuthService , private alertifyService: AlertifyService , private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      userName: new FormControl('' , Validators.required),
-      password: new FormControl('' , [Validators.required , Validators.minLength(4),
-      Validators.maxLength(8)]),
-      confirmPassword: new FormControl('' , Validators.required)
-    }, this.passwordMatchValidator);
+   this.createRegisterForm();
+    // this.registerForm = new FormGroup({
+    //   // gender: new FormControl('male'),
+    //   userName: new FormControl('' , Validators.required),
+    //   password: new FormControl('' , [Validators.required , Validators.minLength(4),
+    //   Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('' , Validators.required)
+    // }, this.passwordMatchValidator);
+   }
+
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+     // gender: ['male'],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      // knownAs: ['', [Validators.required]],
+      // dateOfBirth: [null, [Validators.required]],
+      // city: ['', [Validators.required]],
+      // country: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmpassword: ['', Validators.required]
+    });
   }
 
-  passwordMatchValidator(fg: FormGroup) {
-   return fg.get('password').value === fg.get('confirmPassword').value ? null : { 'mismatch' : true};
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
   }
   register() {
     // this.authService.register(this.model).subscribe(
